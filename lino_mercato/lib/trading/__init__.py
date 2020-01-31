0f2473570bbe10258908aa1417e920ca882f435a
Luc Saffre <luc.saffre@gmail.com>
2020-01-31 13:01:31 +0200

    trading menu config

--- lino_mercato/lib/trading/__init__.py
+++ lino_mercato/lib/trading/__init__.py
@@ -1,24 +1,20 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2014-2020 Rumma & Ko Ltd
+# Copyright 2020 Rumma & Ko Ltd
 # License: BSD (see file COPYING for details)
-
-"""
-An extension of :mod:`lino_xl.lib.contacts`
-"""
 
 from lino.api import ad, _
 
 
 class Plugin(ad.Plugin):
     needs_plugins = ['lino_xl.lib.contacts', 'lino.lib.products']
-    extends_models = ['Partner', 'Person', 'Company']
+    # extends_models = ['Partner', 'Person', 'Company']
 
     # Override to add Workers Menu
     def setup_main_menu(self, site, user_type, m):
         m = m.add_menu(self.app_label, self.verbose_name)
         # We use the string representations and not the classes because
         # other installed applications may want to override these tables.
-        for a in ('contacts.Workers', 'contacts.Companies', 'contacts.Persons'):
+        for a in ('trading.Offers', 'trading.Needs'):
             m.add_action(a)
 
         # a = site.models.contacts.WorkersWeekly
